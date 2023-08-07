5cc30675620cd8c2c13e218cc2ae5ae770e9836c
josuc <josuc@Josue>
2023-08-07 11:58:17 -0600

    .

--- Prueba1_Progra2/src/prueba1_progra2/BlockBusterItem.java
+++ Prueba1_Progra2/src/prueba1_progra2/BlockBusterItem.java
@@ -4,10 +4,37 @@
  */
 package prueba1_progra2;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author josuc
  */
-public class BlockBusterItem {
+public abstract class BlockBusterItem {
+    private int codigo;
+    private String Nombre;
+    private double PrecioRenta;
+    private Date fecha;
+    
+    public BlockBusterItem (int codigo, String Nombre, double PrecioRenta) {
+        this.codigo = codigo;
+        this.Nombre = Nombre;
+        this.PrecioRenta = PrecioRenta;
+        fecha = new Date();
+        toStringFecha();
+    }
+    
+    public String toStringFecha() {     
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        return formato.format(fecha);
+    }
+
+    @Override
+    public String toString() {
+        return "Codigo= " + codigo + "\nNombre= " + Nombre + "\nPreciode Renta= " + PrecioRenta;
+    }
+    
+    public abstract double pagoRenta(int dias);
     
 }
